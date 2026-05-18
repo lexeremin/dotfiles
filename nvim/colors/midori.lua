fc6f0bfe444961aa8d881565c9ee77afba2137c2
Alexander Eremin <dev.lex.eremin@gmail.com>
2026-05-18 23:28:17 +0300

    renamed theme to Midori

--- nvim/colors/midori.lua
+++ nvim/colors/midori.lua
@@ -1,6 +1,6 @@
 vim.cmd("hi clear")
 if vim.fn.exists("syntax_on") == 1 then vim.cmd("syntax reset") end
-vim.g.colors_name = "md-paper"
+vim.g.colors_name = "midori"
 vim.opt.background = "light"
 
 local c = {
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "md-paper" },
+    opts = { colorscheme = "midori" },
   },
 }
